607ea2b60dc00c62ed196be06226afba51dbdacb
Valyuk <val.balinskiy@gmail.com>
2023-04-11 14:51:23 +0300

    navbar

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,11 @@
+import Navbar from "scenes/navbar";
+
+const { Box } = require("@mui/material")
+
+const HomePage = () => {
+    return<Box>
+        <Navbar />
+    </Box>
+}
+
+export default HomePage;
